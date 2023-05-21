411fd69472d75bcc30825c6597f3af75da02b87c
Amy Kate <amykateoc6@gmail.com>
2023-05-20 21:14:04 -0400

    final changes

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const connection = require('./connection');
 
 module.exports = {
     findEmployees() {
-        return connection.promise().query('SELECT * FROM employee')
+        return connection.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;")
     },
     createEmployee(employee) {
         return connection.promise().query('INSERT INTO employee SET ?', employee);
@@ -19,5 +19,8 @@ module.exports = {
     },
     createDepartment(department) {
         return connection.promise().query('INSERT INTO department SET ?', department)
+    },
+    updateEmployeeRole(employee) {
+        return connection.promise().query('UPDATE employee SET role_id = ? WHERE id = ?', [employee.roles, employee.employees])
     }
 }
--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const {
     findEmployees,
     findRoles,
     findDepartment,
-    createDepartment
+    createDepartment,
+    createRole,
+    createEmployee,
+    updateEmployeeRole
 } = require('./db')
 
 const mainQuestion = ([
@@ -19,163 +22,219 @@ const mainQuestion = ([
 
 function addEmployee() {
     findEmployees()
-    .then(([employeeData]) => {
-        console.log(employeeData)
-const employeeChoices = employeeData.map(eachEmployee => ({
-    name: eachEmployee.first_name + ' ' + eachEmployee.last_name,
-    value: eachEmployee.id
-}))
+        .then(([employeeData]) => {
+            console.log(employeeData)
+            const employeeChoices = employeeData.map(eachEmployee => ({
+                name: eachEmployee.first_name + ' ' + eachEmployee.last_name,
+                value: eachEmployee.id
+            }))
+            if (!employeeChoices.length) {
+                employeeChoices.push({ name: 'No employees to choose from', value: null })
+            }
 
-console.log(employeeChoices)
+            findRoles()
+                .then(([roleData]) => {
+                    const roleChoices = roleData.map(eachRole => ({
+                        name: eachRole.title,
+                        value: eachRole.id
+                    }))
 
-const addEmployeeQuestions = ([
-    {
-        message: 'What is the first name?',
-        type: 'input',
-        name: 'first_name',
-    },
-    {
-        message: 'What is the last name?',
-        type: 'input',
-        name: 'last_name'
-    },
-    {
-        message: 'What is the employees role?',
-        type: 'list',
-        choices: [''],
-        name: 'role_id'
-    },
-    {
-        message: 'Who is their manager?',
-        type: 'list',
-        choices: employeeChoices,
-        name: 'manager_id'
-    }
-])
-inquirer.prompt(addEmployeeQuestions)
-.then((answers) => {
-    console.log(answers)
-    createEmployee(answers)
-    .then (() => initialQuestion())
-})
-})
+
+                    console.log(employeeChoices)
+
+                    const addEmployeeQuestions = ([
+                        {
+                            message: 'What is the first name?',
+                            type: 'input',
+                            name: 'first_name',
+                        },
+                        {
+                            message: 'What is the last name?',
+                            type: 'input',
+                            name: 'last_name'
+                        },
+                        {
+                            message: 'What is the employees role?',
+                            type: 'list',
+                            choices: roleChoices,
+                            name: 'role_id'
+                        },
+                        {
+                            message: 'Who is their manager?',
+                            type: 'list',
+                            choices: employeeChoices,
+                            name: 'manager_id'
+                        }
+                    ])
+                    inquirer.prompt(addEmployeeQuestions)
+                        .then((answers) => {
+                            console.log(answers)
+                            createEmployee(answers)
+                                .then(() => initialQuestion())
+                        })
+                })
+        })
 };
 
 function addRole() {
-    findRoles()
-    .then(([roleData]) => {
-        console.log(roleData)
-const deptChoices = roleData.map(eachDept => ({
-    title: eachDept.name,
-    value: department.id
-}))
+    findDepartment()
+        .then(([deptData]) => {
+            console.log(deptData)
+            const deptChoices = deptData.map(eachDept => ({
+                name: eachDept.name,
+                value: eachDept.id
+            }))
 
-console.log(deptChoices)
+            console.log(deptChoices)
 
-const addRoleQuestions = ([
-    {
-        message: 'What is the title of the role?',
-        type: 'input',
-        name: 'title',
-    },
-    {
-        message: 'What is the salary?',
-        type: 'input',
-        name: 'salary'
-    },
-    {
-        message: 'What is the department?',
-        type: 'list',
-        choices: deptChoices,
-        name: 'department_id'
-    }
-])
-inquirer.prompt(addRoleQuestions)
-.then((answers) => {
-    console.log(answers)
-    createRole(answers)
-    .then (() => initialQuestion())
-})
-})
+            const addRoleQuestions = ([
+                {
+                    message: 'What is the title of the role?',
+                    type: 'input',
+                    name: 'title',
+                },
+                {
+                    message: 'What is the salary?',
+                    type: 'input',
+                    name: 'salary'
+                },
+                {
+                    message: 'What is the department?',
+                    type: 'list',
+                    choices: deptChoices,
+                    name: 'department_id'
+                }
+            ])
+            inquirer.prompt(addRoleQuestions)
+                .then((answers) => {
+                    console.log(answers)
+                    createRole(answers)
+                        .then(() => initialQuestion())
+                })
+        })
 };
 
 function addDepartment() {
     findDepartment()
-    .then(([deptData]) => {
-        console.log(deptData)
+        .then(([deptData]) => {
+            console.log(deptData)
 
-const addDeptQuestions = ([
-    {
-        message: 'What is the name of the department?',
-        type: 'input',
-        name: 'name',
-    }
-])
-inquirer.prompt(addDeptQuestions)
-.then((answers) => {
-    console.log(answers)
-    createDepartment(answers)
-    .then (() => initialQuestion())
-})
-})
+            const addDeptQuestions = ([
+                {
+                    message: 'What is the name of the department?',
+                    type: 'input',
+                    name: 'name',
+                }
+            ])
+            inquirer.prompt(addDeptQuestions)
+                .then((answers) => {
+                    console.log(answers)
+                    createDepartment(answers)
+                        .then(() => initialQuestion())
+                })
+        })
 };
 
 function viewEmployees() {
     findEmployees()
-    .then(([employeeData]) => {
-        console.log(employeeData)
-    initialQuestion()
-})
+        .then(([employeeData]) => {
+            console.log(employeeData)
+            initialQuestion()
+        })
 };
 
 function viewRoles() {
     findRoles()
-    .then(([roleData]) => {
-        console.log(roleData)
-    initialQuestion()
-})
+        .then(([roleData]) => {
+            console.log(roleData)
+            initialQuestion()
+        })
 };
 
 function viewDepartments() {
     findDepartment()
-    .then(([departmentData]) => {
-        console.log(departmentData)
-    initialQuestion()
-})
+        .then(([departmentData]) => {
+            console.log(departmentData)
+            initialQuestion()
+        })
+};
+
+function updateEmployee() {
+    findEmployees()
+    .then(([employeeData]) => {
+        console.log(employeeData)
+        const employeeChoices = employeeData.map(eachEmployee => ({
+            name: eachEmployee.first_name + ' ' + eachEmployee.last_name,
+            value: eachEmployee.id
+        }))
+        if (!employeeChoices.length) {
+            employeeChoices.push({ name: 'No employees to choose from', value: null })
+        }
+    findRoles()
+        .then(([roleData]) => {
+            const roleChoices = roleData.map(eachRole => ({
+                name: eachRole.title,
+                value: eachRole.id
+            }))
+            const updateQuestions = ([
+                {
+                    message: "Which employee would you like to update?",
+                    type: 'list',
+                    choices: employeeChoices,
+                    name: 'employees'
+                },
+                {
+                    message: "What is the employee's new role?",
+                    type: 'list',
+                    choices: roleChoices,
+                    name: 'roles'
+                }
+            ])
+            inquirer.prompt(updateQuestions)
+                .then((answers) => {
+                    console.log(answers)
+                    updateEmployeeRole(answers)
+                        .then(() => initialQuestion())
+                })
+        })
+    })
 };
 
 function initialQuestion() {
     inquirer.prompt(mainQuestion)
-    .then ((answers) => {
-        switch(answers.navigation) {
-            case "View all employees":
-                viewEmployees();
-                break;
-            case "Add employee":
-                addEmployee();
-                break;
-            case "Update employee role":
-                updateEmployee();
-            break;
-            case "View all roles":
-                viewRoles();
-            break;
-            case "Add a role":
-                addRole();
-            break;
-            case "View all departments":
-                viewDepartments();
-            break;
-            case "Add a department":
-                addDepartment();
-            break;
-        }
-    })
+        .then((answers) => {
+            switch (answers.navigation) {
+                case "View all employees":
+                    viewEmployees();
+                    break;
+                case "Add employee":
+                    addEmployee();
+                    break;
+                case "Update employee role":
+                    updateEmployee();
+                    break;
+                case "View all roles":
+                    viewRoles();
+                    break;
+                case "Add a role":
+                    addRole();
+                    break;
+                case "View all departments":
+                    viewDepartments();
+                    break;
+                case "Add a department":
+                    addDepartment();
+                    break;
+                case "I'm done":
+                    console.log('Goodbye')
+                    process.exit(0)
+            }
+        })
 };
 
 function init() {
-console.log('Welcome to the Employee Tracker')
-initialQuestion()
+    console.log('Welcome to the Employee Tracker')
+    initialQuestion()
 };
 
 init();
